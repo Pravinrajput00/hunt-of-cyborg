9917f063f351f3ad6e1959212b91eefe4afb7c25
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-09 02:00:17 +0530

    movement animation added

---
+++ Assets/Cybg_Control.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Cybg_Control : MonoBehaviour
+{
+    private Animator mAnimator;
+    public float speed = 2f;
+    public float rotationSpeed = 75f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        if(translation!=0)
+            mAnimator.SetBool("walking", true);
+        else
+        {
+            mAnimator.SetBool("walking", false);
+        }
+    }
+}
