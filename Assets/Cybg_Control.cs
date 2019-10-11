73d69d6a1123d275728c91493895a7f84312cef7
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-12 01:03:08 +0530

    movement animations completed

--- Assets/Cybg_Control.cs
+++ Assets/Cybg_Control.cs
@@ -8,6 +8,7 @@ public class Cybg_Control : MonoBehaviour
     private Animator mAnimator;
     public float speed = 2f;
     public float rotationSpeed = 75f;
+    public float r_speed = 4f;
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -18,30 +19,111 @@ public class Cybg_Control : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-        
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
-        if(translation!=0)
-            mAnimator.SetBool("walking", true);
-        else
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            mAnimator.SetBool("walking", false);
-        }
-        if(Input.GetKey(KeyCode.Space))
-            mAnimator.SetBool("jumping", true);
-        else
-        {
-            mAnimator.SetBool("jumping", false);
-        }
+            speed = r_speed;
+            float translation = Input.GetAxis("Vertical") * speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
 
-        if (translation != 0 && Input.GetKey(KeyCode.Space))
+            transform.Translate(rotation, 0, translation);
+            if (translation != 0)
+                mAnimator.SetBool("running", true);
+            
+            if (Input.GetKey(KeyCode.Space))
+                mAnimator.SetBool("jumping", true);
+            else
+            {
+                mAnimator.SetBool("jumping", false);
+            }
+
+            if (translation != 0 && Input.GetKey(KeyCode.Space))
+            {
+                mAnimator.SetBool("running", true);
+                mAnimator.SetBool("jumping", true);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                mAnimator.SetBool("run_right", true);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                mAnimator.SetBool("run_left", true);
+            }
+
+            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+                mAnimator.SetBool("run_right45", true);
+
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+                mAnimator.SetBool("run_left45", true);
+        }
+        else
         {
-            mAnimator.SetBool("walking", true);
-            mAnimator.SetBool("jumping", true);
+            mAnimator.SetBool("running", false);
+            mAnimator.SetBool("run_right", false);
+            mAnimator.SetBool("run_left45", false);
+            mAnimator.SetBool("run_right45", false);
+            mAnimator.SetBool("run_left", false);
+            float translation = Input.GetAxis("Vertical") * speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+
+            transform.Translate(rotation, 0, translation);
+            if (translation != 0)
+                mAnimator.SetBool("walking", true);
+            else
+            {
+                mAnimator.SetBool("walking", false);
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+                mAnimator.SetBool("jumping", true);
+            else
+            {
+                mAnimator.SetBool("jumping", false);
+            }
+
+            if (translation != 0 && Input.GetKey(KeyCode.Space))
+            {
+                mAnimator.SetBool("walking", true);
+                mAnimator.SetBool("jumping", true);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                mAnimator.SetBool("right", true);
+            }
+            else
+            {
+                mAnimator.SetBool("right", false);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                mAnimator.SetBool("left", true);
+            }
+            else
+            {
+                mAnimator.SetBool("left", false);
+            }
+
+            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+                mAnimator.SetBool("right45", true);
+            else
+            {
+                mAnimator.SetBool("right45", false);
+            }
+
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+                mAnimator.SetBool("left45", true);
+            else
+            {
+                mAnimator.SetBool("left45", false);
+            }
         }
     }
 }
