a80e4166812e9fd605a07a70a74d0102264c720d
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-27 02:23:33 +0530

    collectables placed

---
+++ Assets/Player_Collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Collision : MonoBehaviour
+{
+    public int score=0;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("DragonBall"))
+        {
+            score += 1;
+        }
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public Player_Collision data;
+
+    private void Update()
+    {
+        scoreText.text = data.score.ToString();
+    }
+}
