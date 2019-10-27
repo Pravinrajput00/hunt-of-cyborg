734e7cc0d118d73bd9d6f1fa34d074703d1d76b8
Tanuj Singh <tanujsinghkushwah@gmail.com>
2019-10-27 23:34:49 +0530

    Game Completed! :)

--- Assets/Player_Collision.cs
+++ Assets/Player_Collision.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Player_Collision : MonoBehaviour
 {
     public int score=0;
+    public Text scoreText;
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.CompareTag("DragonBall"))
         {
             score += 1;
         }
+    }
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
     }
 }
---
+++ Assets/timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    private float end = 0f;
+    private float ini=300f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ini != end)
+        {
+            ini -= 1 * Time.deltaTime;
+        }
+
+        timerText.text = ini.ToString("0");
+    }
+}
